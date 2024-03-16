3ee4d7e2d823032625d292d0789527179919c5ad
Kevin Nelson <km.nelson@outlook.com>
2024-03-15 22:50:24 -0400

    Added Clean Up Assignment

--- cleanUp.js
+++ cleanUp.js
@@ -8,37 +8,49 @@
 
 
 // EX 1:
-function askForName() { console.log("Hello, what is your name?") }
+function askForName(name) {
+  return '"Hello, what is your name?"';
+}
+console.log(askForName);
 
-askForName()
-
+// This function is missing parameters, First in askForName you have to define the name of the person
+// that you are asking, which I defined as "name". Then the function was missing is return. Then I had to close the extra spaces between a few
+// brackets. I also corrected the lines in which the code was written, to clean it up.
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
+var sum = first + second + third;
 return sum;
-      }
+}
 
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
+console.log(addThreeNums(1, 2, 3));
+console.log(addThreeNums(4, 2, 7));
 
+// This one was missing the console.log before the two bottom outputs, just having "addThreeNums" alone
+// will not run the function. I also moved the bottom bracket further for cleanliness.
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
 
+// On line 33 corrected the first part and made it function not "func" and added a space between the () and {. 
+// Also changed on line 38 and moved that closing bracket to line 39.
 
 //  EX 4:
-function average(num1, num2) 
-  {
+function average(num1, num2) {
 var sum = num1 + num2;
     var avg = sum / 2;
-
 return avg
-  }
+}
+console.log(average(10, 5));
+
+// I moved the functions opening bracket onto the same line at the functions beginning. I deleted the
+// empty line before the return of the function. I added the console.log to give this function an output.
+// I added in 10 and 5 in order to run the function and test that it properly takes the average of two numbers.
