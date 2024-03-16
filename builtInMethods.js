1eb6a4f719ef60e9ef3bf32e52abb73aa875294c
Kevin Nelson <km.nelson@outlook.com>
2024-03-16 00:03:11 -0400

    Added Built in Method Assignment

--- builtInMethods.js
+++ builtInMethods.js
@@ -10,17 +10,34 @@
 
 
 "Hello World".includes("Hello");
+// The includes() method is called on the string "Hello World"
+// The argument is passed; includes function is to tell whether argument is included within the string.
+// The return value is True
 
 
 "Hello World".endsWith("Hello");
-
+// The endsWith method is called on the string "Hellow World"
+// The argument is passed; endsWith is to tell whether the string ends with the same characters in the argument.
+// The return value is False.
 
 "Hello World".endsWith("rld");
-
+// The endsWith method is called on the string "Hellow World"
+// The argument is passed; endsWith is to tell whether the string ends with the same characters in the argument.
+// The return value is True.
 
 
 // SECTION 2: Calling methods on variables assigned to strings.
 // Declare 2 variables assigned to strings.
+var email = "helloworld@gmail.com"
+
+"email".toUpperCase()
+console.log(toUpperCase(helloworld@gmail.com));
+// Here, using the toUpperCase method, everything written within the string will be written in upper case. The return value will be "HELLOWORLD@GMAIL.COM"
+var mood = 'Happy! '
+
+console.log('I feel ${mood.repeat(2)}');
+// Here, using the .repeat method, everything in the declared variable will be repeated by how many times you specific.
+// The return value would be I feel Happy! Happy!
 // Call a different built-in JavaScript method on each of your variables. 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
 // Include comments above each method call explaining the impact and return value of that method.
@@ -30,8 +47,8 @@
 // The startsWith() method returns true if the data in the firstName variable starts with the argument passed in.
 // In this example, the return value is true, because "Jeff" does start with "J".
 // The console.log() statements prints the return value of the startsWith() method (true) to the console.
-var firstName = "Jeff";
-console.log(firstName.startsWith("J"));
+////var firstName = "Jeff";
+////console.log(firstName.startsWith("J"));
 
 
 
@@ -40,3 +57,15 @@ console.log(firstName.startsWith("J"));
 // Call a different built-in JavaScript method on each of your variables. 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 // Include comments above each method call explaining the impact and return value of that method.
+
+const items = [1, 2, 3];
+console.log(items); // [1, 2, 3]
+
+const reversedItems = items.toReversed();
+console.log(reversedItems); // [3, 2, 1]
+console.log(items); // [1, 2, 3]
+// The method takes the items in the array and reverses their order. The return would be: 3, 2, 1.
+
+var ages = ["90", "50", "20"]
+console.log(ages.copyWithin(0, 2));
+// This method identies the items in the array that you identify within the argument of the method. The return will be 90, 20.
